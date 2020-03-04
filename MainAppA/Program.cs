462578e09ee30594dcf275f8b9b1a2e2212d91d3
王楠 <wangn0729@163.com>
2020-03-04 13:40:06 +0800

    添加LibB

--- MainAppA/Program.cs
+++ MainAppA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LibA;
+using LibB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,8 @@ namespace MainAppA
             Console.WriteLine("This is MainA Application");
             var util = new Util();
             Console.WriteLine(util.Output());
+            var helper = new Helper();
+            Console.WriteLine(helper.Output());
             Console.ReadLine();
         }
     }
